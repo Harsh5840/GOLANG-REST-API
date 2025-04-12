cb4e82f2f926691d56431e1c0bec5ca2739a5c78
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-04-12 17:44:58 +0530

    main

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Harsh5840/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("harsh")
-}
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func (w http.ResponseWriter , r *http.Request)  {
+		w.Write([]byte("welcome to students api"))
+	})
+
+	server := http.Server {
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	fmt.Println("Server started")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server")
+	}
+	
+} 
